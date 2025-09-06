da5a3e3b787d02090ae6997b8c03e95794b62757
AlejoPozzato <alejopozzato@gmail.com>
2025-09-06 20:24:26 -0300

    Agrego rutas para el login y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
 
-function App() {
-  return (
-      <h1>Hola</h1>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/registro" element={<Registro />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
-export default App
