03435188504a57f44d4c553b31bb6298aab6d0a3
AlejoPozzato <alejopozzato@gmail.com>
2025-09-09 22:23:35 -0300

    Agrego getMaterias

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,52 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080/api",
+  headers: {
+      "Content-Type": "application/json",
+    }
 });
 
 export const login = async (email, password) => {
-  const res = await api.post("/autenticacion/login", { email, password });
-  return res.data;
+    try {
+        const res = await api.post("/autenticacion/login", { email, password });
+        console.log("Login exitoso");
+        return res.data;
+    } catch (error) {
+        if (error.response && error.response.data?.error) {
+            throw new Error(error.response.data.error); //mensaje del backend
+        }
+        throw new Error("No se pudo conectar con el servidor.");
+    }
 };
 
 export const registro = async (usuario) => {
-  //usuario = { nombre, mail, password }
-  const res = await api.post("/usuarios/registrar", usuario);
-  return res.data;
+    //usuario = { nombre, email, password }
+    try {
+        const res = await api.post("/usuarios/registrar", usuario);
+        return res.data;
+    } catch (error) {
+        if (error.response && error.response.data?.error) {
+            //mensaje del backend
+            throw new Error(error.response.data.error);
+        }
+        throw new Error("No se pudo conectar con el servidor.");
+    }
 };
+
+export async function getMaterias() {
+    const token = localStorage.getItem("token");
+
+    try {
+        const res = await api.get("/materias", {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
+        });
+        return res.data;
+    } catch (error) {
+        if (error.response && error.response.data?.error) {
+            throw new Error(error.response.data.error); //mensaje del backend
+        }
+        throw new Error("No se pudo conectar con el servidor.");
+    }
+}
